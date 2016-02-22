badd21a83499ac9bcf23f5da814ef91ee0fa3ddc
Henri Koski <henri.t.koski@gmail.com>
2016-02-22 21:14:54 +0200

    Added files

---
+++ cors.go
@@ -0,0 +1,31 @@
+package cors
+
+import "net/http"
+
+const (
+	allow_origin  string = "Access-Control-Allow-Origin"
+	allow_methods string = "Access-Control-Allow-Methods"
+	allow_headers string = "Access-Control-Allow-Headers"
+	origin        string = "Origin"
+	methods       string = "POST, GET, OPTIONS, PUT, DELETE"
+	headers       string = "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token"
+)
+
+type corsHandler struct {
+	h http.Handler
+}
+
+func CORS() func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return &corsHandler{h}
+	}
+}
+
+func (c *corsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if o := r.Header.Get(origin); o != "" {
+		w.Header().Set(allow_origin, o)
+		w.Header().Set(allow_methods, methods)
+		w.Header().Set(allow_headers, headers)
+	}
+	c.h.ServeHTTP(w, r)
+}
---
+++ cors_test.go
@@ -0,0 +1,38 @@
+package cors
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCors(t *testing.T) {
+	req, err := http.NewRequest("OPTIONS", "https://golang.org/", nil)
+	req.Header.Set(origin, "localhost")
+	if err != nil {
+		panic(err)
+	}
+	rr := httptest.NewRecorder()
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wo := w.Header().Get(allow_origin)
+		ro := r.Header.Get(origin)
+		if wo != ro {
+			t.Fatalf("Wrong Allow-Origin '%s' expected '%s'", wo, ro)
+		}
+
+		wm := w.Header().Get(allow_methods)
+		if wm != methods {
+			t.Fatalf("Wrong Allow-Methods '%s' expected '%s'", wm, methods)
+		}
+
+		wh := w.Header().Get(allow_headers)
+		if wh != headers {
+			t.Fatalf("Wrong Allow-Header '%s' expected '%s'", wh, headers)
+		}
+	})
+
+	CORS()(h).ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatal(http.StatusFound)
+	}
+}
