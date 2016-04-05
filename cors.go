42a83c14130904afcf3d3ce80dae434279ecc40a
henkosk <henri.koski@codemate.com>
2016-04-05 17:05:31 +0300

    Added expose header to allow api client to access header data

--- cors.go
+++ cors.go
@@ -8,10 +8,13 @@ const (
 	allow_methods     string = "Access-Control-Allow-Methods"
 	allow_headers     string = "Access-Control-Allow-Headers"
 	allow_credentials string = "Access-Control-Allow-Credentials"
+	expose_headers    string = "Access-Control-Expose-Headers"
 	credentials       string = "true"
 	origin            string = "Origin"
 	methods           string = "POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH"
-	headers           string = "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token"
+
+	// If you want to expose some other headers add it here
+	headers string = "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token"
 )
 
 type corsHandler struct {
@@ -25,16 +28,20 @@ func CORS() func(http.Handler) http.Handler {
 }
 
 func (c *corsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Set allow origin to match origin of our request or fall back to *
 	if o := r.Header.Get(origin); o != "" {
 		w.Header().Set(allow_origin, o)
 	} else {
 		w.Header().Set(allow_origin, "*")
 	}
 
+	// Set other headers
 	w.Header().Set(allow_headers, headers)
-	w.Header().Set(allow_credentials, credentials)
 	w.Header().Set(allow_methods, methods)
+	w.Header().Set(allow_credentials, credentials)
+	w.Header().Set(expose_headers, headers)
 
+	// If this was preflight options request let's write empty ok response and return
 	if r.Method == options {
 		w.WriteHeader(http.StatusOK)
 		w.Write(nil)
